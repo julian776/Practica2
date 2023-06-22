106e1a54797d0a9b2378d2c7b0c18492a7d70dcb
amgg16 <124928492+amgg16@users.noreply.github.com>
2023-06-22 13:51:02 -0500

    .

--- main.py
+++ main.py
@@ -6,13 +6,18 @@ from datetime import date, time, datetime
 from baseDatos.serializador import Serializador
 from baseDatos.deserializador import Deserializador
 
-# Creación del usuario administrador
-usuario = Usuario("1","Administrador","administrador@unal.edu.co")
-serializar = Serializador(usuario)
-serializar.serializar()
+#Se deserializa el usuario administrador
+deserializar = Deserializador()
+deserializar.deserializar()
+usuario = deserializar.getArgs()
 
 # Ventana de inicio
 ventana =  VentanaInicio(usuario)
 
 # Ejecuta la ventana
 ventana.mainloop()
+
+#Creación del usuario administrador
+#usuario = Usuario("1","Administrador","administrador@unal.edu.co",date.today(),"12345")
+#serializar = Serializador(usuario)
+#serializar.serializar()
